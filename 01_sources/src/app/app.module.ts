6b8e0768ad78273c43e3e0a0753eed4fb573732f
Jbristhuille <jbristhuille@gmail.com>
2022-10-03 12:46:30 +0200

    :fire: Remove routing module

--- 01_sources/src/app/app.module.ts
+++ 01_sources/src/app/app.module.ts
@@ -15,8 +15,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppRoutingModule } from './app-routing.module';
 /***/
 
 /* Components */
@@ -25,7 +23,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
