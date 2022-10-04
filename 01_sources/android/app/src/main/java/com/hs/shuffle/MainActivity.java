52ee9ca4019349767e0edf66cc603c6918c06bed
Jbristhuille <jbristhuille@gmail.com>
2022-10-04 12:18:34 +0200

    :construction_worker: Setup android build

---
+++ 01_sources/android/app/src/main/java/com/hs/shuffle/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hs.shuffle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- 01_sources/capacitor.config.ts
+++ 01_sources/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'hs.shuffle',
-  appName: 'shuffle',
+  appId: 'com.hs.shuffle',
+  appName: 'Shuffle: Random Color Generator',
   webDir: 'www',
   bundledWebRuntime: false
 };
